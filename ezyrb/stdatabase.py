85d411db8c2033f24ff7ad4933eb1488467526b0
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2021-03-23 16:21:31 +0100

    introduce STDatabase

---
+++ ezyrb/stdatabase.py
@@ -0,0 +1,130 @@
+"""
+Module for the snapshots database collected during the Offline stage
+"""
+import numpy as np
+
+
+class STDatabase(object):
+    """
+    Database class
+
+    :param array_like parameters: the input parameters
+    :param array_like time_instants: the time instants
+    :param numpy.ndarray snapshots: the input snapshots, space varies along the
+        first axis, time along the second, and parameters along the third axis.
+    :param Scale scaler_parameters: the scaler for the parameters. Default
+        is None meaning no scaling.
+    :param Scale scaler_snapshots: the scaler for the snapshots. Default is
+        None meaning no scaling.
+    """
+    def __init__(self,
+                 parameters=None,
+                 time_instants=None,
+                 snapshots=None,
+                 scaler_parameters=None,
+                 scaler_snapshots=None):
+        self._time_instants = None
+        self._parameters = None
+        self._snapshots = None
+        self.scaler_parameters = scaler_parameters
+        self.scaler_snapshots = scaler_snapshots
+
+        # if only parameters or snapshots are provided
+
+        provided = 0
+        if parameters is not None:
+            provided += 1
+        if time_instants is not None:
+            provided += 1
+        if snapshots is not None:
+            provided += 1
+
+        if provided != 0 and provided != 3:
+            raise RuntimeError('Wrong number of arguments')
+
+        if parameters is not None and snapshots is not None:
+            self.add(parameters, time_instants, snapshots)
+
+    @property
+    def parameters(self):
+        """
+        The matrix containing the input parameters (by row).
+
+        :rtype: numpy.ndarray
+        """
+        if self.scaler_parameters:
+            return self.scaler_parameters.fit_transform(self._parameters)
+        else:
+            return self._parameters
+
+    @property
+    def time_instants(self):
+        """
+        The array containing the time instants.
+
+        :rtype: numpy.ndarray
+        """
+        return self._time_instants
+
+    @property
+    def snapshots(self):
+        """
+        The matrix containing the snapshots (by row).
+
+        :rtype: numpy.ndarray
+        """
+        if self.scaler_snapshots:
+            return self.scaler_snapshots.fit_transform(self._snapshots)
+        else:
+            return self._snapshots
+
+    def __getitem__(self, val):
+        """
+        This method returns a new Database with the selected parameters and snapshots.
+
+        .. warning:: The new parameters and snapshots are a view of the
+            original Database.
+        """
+        return STDatabase(parameters=self._parameters[val],
+            time_instants=self._time_instants, snapshots=self._snapshots[val],
+            scaler_parameters=self.scaler_parameters,
+            scaler_snapshots=self.scaler_snapshots)
+
+    def __len__(self):
+        """
+        This method returns the number of snapshots.
+
+        :rtype: int
+        """
+        return self._snapshots.shape[1] * self._snapshots.shape[2]
+
+    def add(self, parameters, time_instants, snapshots):
+        """
+        Add (by row) new sets of snapshots and parameters to the original
+        database.
+
+        :param array_like parameters: the parameters to add.
+        :param array_like time_instants: the time instants to add.
+        :param numpy.ndarray snapshots: the snapshots to add.
+        """
+        # check dimensions and type
+        if not isinstance(snapshots, np.ndarray) or snapshots.ndim != 3:
+            raise ValueError('The provided snapshot must be a 3D nparray')
+
+        if (snapshots.shape[0] != len(parameters) or
+            snapshots.shape[2] != len(time_instants)):
+            raise ValueError("""Dimensions do not agree.\nsnapshots: {} time
+                instants for {} parameters, but received {} time instants
+                and {} parameters""" .format(snapshots.shape[2],
+                snapshots.shape[0], len(time_instants), len(parameters)))
+
+        if (self._parameters is None and self._time_instants is None and
+            self._snapshots is None):
+            self._parameters = parameters
+            self._time_instants = time_instants
+            self._snapshots = snapshots
+        else:
+            self._parameters = np.vstack([self._parameters, parameters])
+            self._snapshots = np.vstack([self._snapshots, snapshots])
+
+        return self
---
+++ tests/test_stdatabase.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+from unittest import TestCase
+from ezyrb import STDatabase
+
+
+class TestStDatabase(TestCase):
+    def test_constructor_empty(self):
+        a = STDatabase()
+
+    def test_constructor_arg(self):
+        db = STDatabase(np.random.uniform(size=(10, 3)),
+                 np.random.uniform(size=(100)),
+                 np.random.uniform(size=(10, 50, 100)))
+
+        assert db.parameters.shape == (10, 3)
+        assert db.time_instants.shape == (100,)
+        assert db.snapshots.shape == (10,50,100)
+
+    def test_constructor_arg_wrong(self):
+        with self.assertRaises(ValueError):
+            STDatabase(np.random.uniform(size=(10, 3)),
+                 np.random.uniform(size=(100)),
+                 np.random.uniform(size=(9, 50, 100)))
+
+    def test_constructor_arg_wrong2(self):
+        with self.assertRaises(ValueError):
+            STDatabase(np.random.uniform(size=(10, 3)),
+                 np.random.uniform(size=(100)),
+                 np.random.uniform(size=(10, 50, 101)))
+
+    def test_constructor_error(self):
+        with self.assertRaises(RuntimeError):
+            STDatabase(np.eye(5))
+
+    def test_constructor_error2(self):
+        with self.assertRaises(RuntimeError):
+            STDatabase(np.eye(5), np.eye(5))
+
+    def test_getitem(self):
+        org = STDatabase(np.random.uniform(size=(10, 3)),
+                 np.random.uniform(size=(100)),
+                 np.random.uniform(size=(10, 50, 100)))
+        new = org[::2]
+
+        assert new.parameters.shape[0] == 5
+        assert len(new.time_instants) == 100
+        assert new.snapshots.shape == (5,50,100)
