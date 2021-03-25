f065d702fcfe0584c2c8c8c0333fc473c4347890
francescoandreuzzi <andreuzzi.francesco@gmail.com>
2021-03-25 11:53:59 +0100

    minor fixes

--- ezyrb/spacetime_pod.py
+++ ezyrb/spacetime_pod.py
@@ -28,16 +28,32 @@ class SpaceTimePOD(Reduction):
         # - 0 -> time
         # - 1 -> space
         # - 2 ->  parameters
-        X2 = np.reshape(X, (X.shape[0], X.shape[1] * X.shape[2]), 'F')
+
+        # temp axes:
+        # - 0 -> space
+        # - 1 -> time
+        # - 2 ->  parameters
         temp = np.swapaxes(X, 0, 1)
+
+        # we store this values to use later when reconstructing the snapshots
+        # in reduce()
+        self._space_points = temp.shape[0]
+        self._time_instants = temp.shape[1]
+        self._ntrain = temp.shape[2]
+
         X1 = np.reshape(temp, (temp.shape[0], temp.shape[1] * temp.shape[2]), 'F')
+        X2 = np.reshape(X, (X.shape[0], X.shape[1] * X.shape[2]), 'F')
 
         if self._tailored:
             self._tailored_spacetime_basis(X1, X2)
         else:
             self._standard_spacetime_basis(X1, X2)
 
-        return self.modes.T.dot(X)
+        # rows: space/time
+        # columns: parameters
+        X3 = np.reshape(temp, (temp.shape[0] * temp.shape[1], temp.shape[2]), 'F')
+
+        return self.modes.T.dot(X3)
 
     def _standard_spacetime_basis(self, X1, X2):
         spatial_pod = POD(**self._spatial_pod_args)
@@ -48,25 +64,24 @@ class SpaceTimePOD(Reduction):
         temporal_pod.reduce(X2)
         Psi = temporal_pod.modes
 
-        self._modes = np.kron(Phi, Psi)
+        self._modes = np.kron(Psi, Phi)
 
     def _tailored_spacetime_basis(self, X1, X2):
         spatial_pod = POD(**self._spatial_pod_args)
         spatial_pod.reduce(X1)
         Phi = spatial_pod.modes
 
-        ntrain = int(X1.shape[1] / X2.shape[0])
+        self._temporal_pod = POD(**self._temporal_pod_args)
+        self._modes = np.hstack([self._tailored_basis_piece(spatial_mode=u,
+            X2=X2) for u in Phi.T])
 
-        temporal_pod = POD(**self._temporal_pod_args)
-        self._modes = np.block([self._tailored_basis_piece(spatial_mode=u, X2=X2,
-            temporal_pod=temporal_pod, ntrain=ntrain) for u in Phi.T])
-
-    def _tailored_basis_piece(self, spatial_mode, X2, temporal_pod, ntrain):
-        XPhi_i = np.block([X2[:, i * spatial_mode.shape[0]] @ spatial_mode
-            for i in range(ntrain)])
-        temporal_pod.reduce(XPhi_i)
-        tailored_Psi_i = temporal_pod.modes
-        return np.kron(spatial_mode, tailored_Psi_i)
+    def _tailored_basis_piece(self, spatial_mode, X2):
+        X2Phi_i = np.hstack((X2[:, i * spatial_mode.shape[0] : (i + 1) *
+            spatial_mode.shape[0]] @ spatial_mode)[:, None]
+            for i in range(self._ntrain))
+        self._temporal_pod.reduce(X2Phi_i)
+        tailored_Psi_i = self._temporal_pod.modes
+        return np.kron(tailored_Psi_i, spatial_mode[:,None])
 
     def expand(self, X):
         """
@@ -74,4 +89,13 @@ class SpaceTimePOD(Reduction):
 
         :type: numpy.ndarray
         """
-        return self.modes.dot(X).T
+        expanded = self.modes.dot(X).T
+
+        if X.ndim == 2:
+            # X contains modal coefficients for two or more parameters
+            space_time = np.reshape(expanded, (self._space_points,
+                self._time_instants, X.shape[1]), 'F')
+        else:
+            space_time = np.reshape(expanded, (self._space_points,
+                self._time_instants), 'F')
+        return np.swapaxes(space_time, 0,1)
